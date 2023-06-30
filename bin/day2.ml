ad7df46d1fce8daa8fed6486a39a23230cb03587
Erick Fuentes <fuentes.erick@gmail.com>
2023-06-30 06:52:18 -0400

    clean up

--- bin/day2.ml
+++ bin/day2.ml
@@ -5,10 +5,10 @@ type outcome = Win | Loss | Draw
 
 let score_round (a, b) =
   let match_result =
-    match a with
-    | Rock -> ( match b with Rock -> 3 | Paper -> 6 | Scissors -> 0)
-    | Paper -> ( match b with Rock -> 0 | Paper -> 3 | Scissors -> 6)
-    | Scissors -> ( match b with Rock -> 6 | Paper -> 0 | Scissors -> 3)
+    match (a, b) with
+    | Paper, Scissors | Scissors, Rock | Rock, Paper -> 6
+    | Rock, Rock | Paper, Paper | Scissors, Scissors -> 3
+    | Scissors, Paper | Paper, Rock | Rock, Scissors -> 0
   in
   (match b with Rock -> 1 | Paper -> 2 | Scissors -> 3) + match_result
 
@@ -17,25 +17,24 @@ let move_of_str_a move_str =
   match c with
   | 'A' -> Rock
   | 'B' -> Paper
-  | 'C' -> Scissors 
-  | _ -> raise (Stdlib.Invalid_argument ("Unknown string" ^ String.of_char c))
+  | 'C' -> Scissors
+  | _ -> failwith ("Unknown string" ^ Char.to_string c)
 
 let move_of_str_b move_str =
   let c = String.get move_str 0 in
   match c with
   | 'X' -> Rock
   | 'Y' -> Paper
-  | 'Z' -> Scissors 
-  | _ -> raise (Stdlib.Invalid_argument ("Unknown string" ^ String.of_char c))
+  | 'Z' -> Scissors
+  | _ -> failwith ("Unknown string" ^ Char.to_string c)
 
 let outcome_of_str outcome_str =
   let c = String.get outcome_str 0 in
   match c with
   | 'X' -> Loss
-  | 'Y' -> Draw 
-  | 'Z' -> Win 
-  | _ -> raise (Stdlib.Invalid_argument ("Unknown string" ^ String.of_char c))
-
+  | 'Y' -> Draw
+  | 'Z' -> Win
+  | _ -> failwith ("Unknown string" ^ Char.to_string c)
 
 let parse_line line =
   let left, right = String.lsplit2_exn line ~on:' ' in
@@ -50,27 +49,25 @@ let parse_outcome_line line =
   (left_move, outcome)
 
 let moves_from_outcome (move, outcome) =
-  let my_move = 
-    match outcome with
-    | Draw -> (match move with Rock -> Rock | Paper -> Paper | Scissors -> Scissors)
-    | Win -> (match move with Rock -> Paper | Paper -> Scissors | Scissors -> Rock)
-    | Loss -> (match move with Rock -> Scissors | Paper -> Rock | Scissors -> Paper)
+  let my_move =
+    match (move, outcome) with
+    | Rock, Draw | Paper, Loss | Scissors, Win -> Rock
+    | Rock, Win | Paper, Draw | Scissors, Loss -> Paper
+    | Rock, Loss | Paper, Win | Scissors, Draw -> Scissors
   in
-    (move, my_move)
+  (move, my_move)
 
 let () =
+  let lines = In_channel.read_lines "input/day2.txt" in
   let initial_score =
-    In_channel.read_lines "input/day2.txt"
-    |> List.map ~f:parse_line |> List.map ~f:score_round
+    lines |> List.map ~f:parse_line |> List.map ~f:score_round
     |> List.reduce_exn ~f:( + )
   in
   print_endline (string_of_int initial_score);
-
   let final_score =
-    In_channel.read_lines "input/day2.txt"
+    lines
     |> List.map ~f:parse_outcome_line
     |> List.map ~f:moves_from_outcome
-    |>  List.map ~f:score_round
-    |> List.reduce_exn ~f:( + )
+    |> List.map ~f:score_round |> List.reduce_exn ~f:( + )
   in
-  print_endline (string_of_int final_score);
+  print_endline (string_of_int final_score)
