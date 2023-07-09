ea3762cb2bb5d43081ffd03d59bf7460d5ebe616
Erick Fuentes <fuentes.erick@gmail.com>
2023-07-09 13:33:45 -0400

    finish day 13

--- bin/day12.ml
+++ bin/day12.ml
@@ -42,7 +42,7 @@ let print_array cost_map =
       Array.iter row ~f:(fun item -> Out_channel.printf " %04d" item);
       Out_channel.newline stdout)
 
-let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
+let djikstra ?(step_cost = 1) ?(travel_dir = Uphill) grid (x_a, y_a) =
   let num_rows = Array.length grid in
   let num_cols = Array.length grid.(0) in
   let out = Array.make_matrix ~dimx:num_rows ~dimy:num_cols 1000 in
@@ -52,9 +52,11 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
     let did_update = ref false in
     Array.iteri grid ~f:(fun row_idx row ->
         Array.iteri row ~f:(fun col_idx item ->
-            let min_height_delta, max_height_delta = match travel_dir with
-            | Uphill -> Int.min_value, 1
-            | Downhill -> -1, Int.max_value in
+            let min_height_delta, max_height_delta =
+              match travel_dir with
+              | Uphill -> (Int.min_value, 1)
+              | Downhill -> (-1, Int.max_value)
+            in
             (* check neighbors *)
             (* Check above *)
             (if row_idx > 0 then
@@ -63,7 +65,8 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
                  out.(row_idx).(col_idx) - out.(row_idx - 1).(col_idx)
                in
                if
-                 Int.between ~low:min_height_delta ~high:max_height_delta height_delta
+                 Int.between ~low:min_height_delta ~high:max_height_delta
+                   height_delta
                  && not (cost_delta <= step_cost)
                then (
                  did_update := true;
@@ -77,7 +80,8 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
                  out.(row_idx).(col_idx) - out.(row_idx + 1).(col_idx)
                in
                if
-                 Int.between ~low:min_height_delta ~high:max_height_delta height_delta
+                 Int.between ~low:min_height_delta ~high:max_height_delta
+                   height_delta
                  && not (cost_delta <= step_cost)
                then (
                  did_update := true;
@@ -90,7 +94,8 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
                  out.(row_idx).(col_idx) - out.(row_idx).(col_idx + 1)
                in
                if
-                 Int.between ~low:min_height_delta ~high:max_height_delta height_delta
+                 Int.between ~low:min_height_delta ~high:max_height_delta
+                   height_delta
                  && not (cost_delta <= step_cost)
                then (
                  did_update := true;
@@ -103,7 +108,8 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
                 out.(row_idx).(col_idx) - out.(row_idx).(col_idx - 1)
               in
               if
-                 Int.between ~low:min_height_delta ~high:max_height_delta height_delta
+                Int.between ~low:min_height_delta ~high:max_height_delta
+                  height_delta
                 && not (cost_delta <= step_cost)
               then (
                 did_update := true;
@@ -114,24 +120,22 @@ let djikstra ?(step_cost = 1) ?(travel_dir=Uphill) grid (x_a, y_a) =
   update_helper out
 
 let find_nearest_low_point grid value_func =
-  let min_loc = ref (0,0) in
+  let min_loc = ref (0, 0) in
   let min_val = ref 1000 in
   Array.iteri grid ~f:(fun row_idx row ->
-    Array.iteri row ~f:(fun col_idx item ->
-      let value = value_func.(row_idx).(col_idx) in
-      if item = 0 && value < !min_val then (
-        min_loc := row_idx, col_idx;
-        min_val := value;
-      )
-    ));
-  !min_loc, !min_val
+      Array.iteri row ~f:(fun col_idx item ->
+          let value = value_func.(row_idx).(col_idx) in
+          if item = 0 && value < !min_val then (
+            min_loc := (row_idx, col_idx);
+            min_val := value)));
+  (!min_loc, !min_val)
 
 let () =
   let lines = In_channel.read_lines "input/day12.txt" in
   let grid, start_loc, stop_loc = parse_lines lines in
   let value_func = djikstra grid start_loc in
-  let (stop_x, stop_y) = stop_loc in
+  let stop_x, stop_y = stop_loc in
   print_endline (Int.to_string value_func.(stop_x).(stop_y));
   let value_func = djikstra ~travel_dir:Downhill grid stop_loc in
   let _, lowest_value = find_nearest_low_point grid value_func in
-  print_endline (Int.to_string lowest_value) 
+  print_endline (Int.to_string lowest_value)
--- bin/day13.ml
+++ bin/day13.ml
@@ -1,4 +1,3 @@
-
 open Core
 
 type packet = Value of int | Packet of packet list
@@ -7,28 +6,66 @@ let rec sexp_to_packet = function
   | Sexp.Atom s -> Value (Int.of_string s)
   | Sexp.List l -> Packet (List.map l ~f:sexp_to_packet)
 
+let rec packet_to_sexp = function
+  | Value i -> Sexp.Atom (Int.to_string i)
+  | Packet l -> Sexp.List (List.map l ~f:packet_to_sexp)
 
 let parse_packet line =
-  if String.is_empty line then
-    None
+  if String.is_empty line then None
   else
-    let packet_str = String.substr_replace_all ~pattern:"[" ~with_:"(" line in
-    let packet_str = String.substr_replace_all ~pattern:"]" ~with_:")" packet_str in
-    let packet_str = String.substr_replace_all ~pattern:"," ~with_:" " packet_str in
-    let sexp = Sexp.of_string packet_str in
-    let packet = sexp_to_packet sexp in
+    let packet =
+      String.substr_replace_all ~pattern:"[" ~with_:"(" line
+      |> String.substr_replace_all ~pattern:"]" ~with_:")"
+      |> String.substr_replace_all ~pattern:"," ~with_:" "
+      |> Sexp.of_string |> sexp_to_packet
+    in
     Some packet
 
-
-let parse_lines lines: packet list list =
-  let packets = List.filter_map lines ~f:(
-    fun line -> parse_packet line
-  ) in
-  List.chunks_of ~length: 2 packets
-  
-
+let rec compare (a : packet) (b : packet) =
+  match (a, b) with
+  | Value _, _ -> failwith "Left had only Value"
+  | _, Value _ -> failwith "Right had only Value"
+  | Packet [], Packet [] -> 0
+  | Packet [], Packet (_ :: _) -> -1 (* left ran out first *)
+  | Packet (_ :: _), Packet [] -> 1 (* right ran out first *)
+  | Packet (Value l :: lrest), Packet (Value r :: rrest) ->
+      if l < r then -1
+      else if r < l then 1
+      else compare (Packet lrest) (Packet rrest)
+  | Packet (Value l :: lrest), Packet (Packet r :: rrest) ->
+      let result = compare (Packet [ Value l ]) (Packet r) in
+      if result <> 0 then result else compare (Packet lrest) (Packet rrest)
+  | Packet (Packet l :: lrest), Packet (Value r :: rrest) ->
+      let result = compare (Packet l) (Packet [ Value r ]) in
+      if result <> 0 then result else compare (Packet lrest) (Packet rrest)
+  | Packet (Packet l :: lrest), Packet (Packet r :: rrest) ->
+      let result = compare (Packet l) (Packet r) in
+      if result <> 0 then result else compare (Packet lrest) (Packet rrest)
 
 let () =
-  let lines = In_channel.read_lines "input/day13_test.txt" in
-  let packet_pairs = parse_lines lines in
-  print_endline "Hello World!"
+  In_channel.read_lines "input/day13.txt"
+  |> List.filter_map ~f:(fun line -> parse_packet line)
+  |> List.chunks_of ~length:2
+  |> List.map ~f:(fun a -> (List.hd_exn a, List.last_exn a))
+  |> List.mapi ~f:(fun idx (a, b) ->
+         let result = compare a b in
+         if result < 0 then idx + 1 else 0)
+  |> List.reduce_exn ~f:( + ) |> Int.to_string |> print_endline;
+
+  let divider_packet_1 = sexp_to_packet (Sexp.of_string "((2))") in
+  let divider_packet_2 = sexp_to_packet (Sexp.of_string "((6))") in
+  let div_packets = [divider_packet_1; divider_packet_2] in
+  In_channel.read_lines "input/day13.txt"
+  |> List.filter_map ~f:(fun line -> parse_packet line)
+  |> List.append div_packets
+  |> List.sort ~compare: compare
+  |> List.filter_mapi ~f:(fun idx p -> 
+                  if (Sexp.equal (packet_to_sexp p) (packet_to_sexp divider_packet_1)) || 
+                  (Sexp.equal (packet_to_sexp p) (packet_to_sexp divider_packet_2)) then
+                          Some (idx + 1)
+                  else None)
+  |> List.reduce_exn ~f:( * )
+  |> Int.to_string
+  |> print_endline
+                  
+
